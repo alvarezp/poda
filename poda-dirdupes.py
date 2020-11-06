384ec390b4a3e73bd9f100d229e8e35123b3a7ac
Octavio Alvarez <octalgl@alvarezp.org>
2020-11-05 22:34:36 -0600

    Complete rework of poda-dirdupes.py

--- poda-dirdupes.py
+++ poda-dirdupes.py
@@ -7,106 +7,101 @@ import fileinput
 import itertools
 import sys
 
-def similarity(dir1totalsize, dir2totalsize, equalcontentsize):
-    if dir1totalsize + dir2totalsize == 0:
-        return 1
-    return 2 * equalcontentsize / (dir1totalsize + dir2totalsize)
+dirname = os.path.dirname
+combinations = itertools.combinations
 
-def dir1_contains_or_is_dir2(dir1, dir2):
-    return dir2[0:len(dir1)] == dir1
+def dprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def similarity(dir1totalsize, dir2totalsize, equalcontentsize):
+    try:
+        return (equalcontentsize + equalcontentsize) / (dir1totalsize + dir2totalsize)
+    except ZeroDivisionError:
+        return 1
+
+def processclass(direquals, dirsizes, pathamounts, filesize):
+    #dprint("!!! Entering processclass...")
+    pathlist = list()
+    for path in pathamounts:
+        splitpath = path.split('/')
+        pathlist.extend(map("/".join, [splitpath[:i+1] for i in range(len(splitpath))]))
+        amount = pathamounts[path]
+        for p in map("/".join, [splitpath[:i+1] for i in range(len(splitpath))]):
+            try:
+                dirsizes[p] += amount * filesize
+                #dprint("!!! dirsizes['%s'] += %d * %d" % (path, amount, filesize))
+            except KeyError:
+                dirsizes[p] = amount * filesize
+
+    pathset = sorted(set(pathlist))
+    #dprint("!!! generated path set: %s" % (str(pathset)))
+
+    tuplelist = [(d1, d2) for (d1, d2) in combinations(pathset, 2) if not d2[0:len(d1)] == d1]
+    #dprint("tuplelist: %s" % (str(tuplelist)))
+
+    for t in tuplelist:
+        # 1. Find out how many times the file occurs in each of the
+        #    directories in the tuple
+        # 2. Consider the minimum of the two to be the "common" amount for
+        #    direquals (similarity).
+        t0 = 0
+        t1 = 0
+        for p in paths:
+            if p.startswith(t[0]):
+                t0 = t0 + paths[p]
+            if p.startswith(t[1]):
+                t1 = t1 + paths[p]
+        common = min(t0, t1)
+        #dprint("! == common: %d %d %d" % (t0, t1, common))
+
+        try:
+            direquals[t] += common * filesize
+        except KeyError:
+            direquals[t] = common * filesize
+        #dprint("!!! -- -- direquals[%s] += %d * %d = %d" % (str(t), common, filesize, direquals[t]))
 
 dirsizes = dict()
 direquals = dict()
-equalpathpool = list()
-nextdirequals = set()
-equalpathtotals = dict()
+
+paths = dict()
+
+filesize = 0
+
+prevline = "================================ 000000000000 xxxx-xx-xx xx:xx:xx +xxxx xxx xxx ./whatever\n"
+n = 0
 
 with open(0, 'r', errors='replace') as f:
-
-    prevline = "================================ 000000000000 xxxx-xx-xx xx:xx:xx +xxxx xxx xxx ./whatever\n"
-    n = 0
-    while True:
-
-        line = f.readline()
-        if line != "":
-            n = n + 1
-            #print(n)
-            #print("+ " + line.rstrip())
-
-            try:
-                size = int(line.split(" ")[1])
-            except:
-                print("BAD LINE: " + line)
-                raise
-                
-            path = ":".join([line.split(" ")[5], line.split(" ")[6], os.path.dirname(" ".join(line.split(" ")[7:]))])
-            
-            while True:
-                if path in dirsizes:
-                    dirsizes[path] += size
-                else:
-                    dirsizes[path] = size
-                #print("! dirsizes['%s'] += %s" % (path, size))
-                path = os.path.dirname(path)
-                if path == "/" or path == "":
-                    break
-
-        prevsize = int(prevline.split(" ")[1])
-        prevpath = ":".join([prevline.split(" ")[5], prevline.split(" ")[6], os.path.dirname(" ".join(prevline.split(" ")[7:]))])
-
-        # add the path from prevline to an "equal pool"
-        if prevpath not in equalpathpool:
-            equalpathpool.append(prevpath)
-            #print("! equalpathpool.append('%s')" % (prevpath))
-            while True:
-                if prevpath in equalpathtotals:
-                    equalpathtotals[prevpath] += prevsize
-                else:
-                    equalpathtotals[prevpath] = prevsize
-                ##print("! equalpathtotals['%s'] += %s" % (prevpath, prevsize))
-                prevpath = os.path.dirname(prevpath)
-                if prevpath == "/" or prevpath == "":
-                    break
-
+    for line in f:
         # - The equal pool will be processed once a
         #   **differing-content** line is found.
-        # When the equal pool is processed:
-        # - equal[./a/b][./c/d] += size per path combination
-        #   from the equal pool, but if c and d are
-        #   subdirs, then the parent dirs will also
-        #   have the size added, thus:
-        #   - equal[./a][/c] += size, also.
         if line[0:51] != prevline[0:51]:
-            #print("! processing equalpathpool: " + str(equalpathpool))
-            combs = itertools.combinations(sorted(equalpathpool), 2)
-            for combelems in combs:
-                path1 = combelems[0]
-                path2 = combelems[1]
-                while True:
-                    dirtuple = tuple(sorted(list((path1, path2))))
-                    if dir1_contains_or_is_dir2(dirtuple[0], dirtuple[1]):
-                        break
-                    nextdirequals.add(dirtuple)
-                    path1 = os.path.dirname(dirtuple[0])
-                    path2 = os.path.dirname(dirtuple[1])
-                    if path1 == "/" or path1 == "" or \
-                        path2 == "/" or path2 == "":
-                        break
-            for p in nextdirequals:
-                if p in direquals:
-                    direquals[p] += min(equalpathtotals[p[0]], equalpathtotals[p[1]])
-                else:
-                    direquals[p] = min(equalpathtotals[p[0]], equalpathtotals[p[1]])
-                #print("! direquals[%s] += %s" % (str(p), min(equalpathtotals[p[0]], equalpathtotals[p[1]])))
+            processclass(direquals, dirsizes, paths, filesize)
+            paths.clear()
 
-            #print("! equalpathpool.clear()")
-            equalpathpool.clear()
-            nextdirequals.clear()
-            equalpathtotals.clear()
+        #dprint("+ " + line.rstrip())
+
+        try:
+            filesize = int(line.split(" ")[1])
+        except:
+            print("BAD LINE: " + line)
+            raise
+        #dprint("! filesize: %d" % (filesize))
+
+        # Converts "host hamper ./path to/some/file.txt"
+        # ... to "host:hamper:./path to/some"
+        path = ":".join([line.split(" ")[5], line.split(" ")[6], dirname(" ".join(line.split(" ")[7:]))])
+        #dprint("! path: %s" % (path))
+
+        try:
+            paths[path] += 1
+        except KeyError:
+            paths[path] = 1
 
         prevline = line
-        if line == "":
-            break
+
+    else:
+        processclass(direquals, dirsizes, paths, filesize)
+
 
 for p in direquals:
     ##print("! p = %s" % (str(p)))
